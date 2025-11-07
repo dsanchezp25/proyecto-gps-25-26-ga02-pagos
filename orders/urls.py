40f8ee877339df76f3c8d2e5d18100b7782f755e
dsanchezp25 <danisanchezp25@gmail.com>
2025-11-07 19:15:33 +0100

    refactor(orders): Alinear vistas y URLs de Orders con OpenAPI

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet
+from .views import OrderListCreateAPIView, OrderRetrieveAPIView
 
-router = DefaultRouter()
-router.register(r'orders', OrderViewSet, basename='order')
+
+# POST /api/v1/orders
+# GET /api/v1/orders/{order_id}
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('orders/',
+         OrderListCreateAPIView.as_view(),
+         name='order-list-create'),
+
+    path('orders/<uuid:order_id>/',
+         OrderRetrieveAPIView.as_view(),
+         name='order-retrieve'),
+
+    # TODO: Aún nos falta el endpoint para descargar el PDF.
+    # Lo añadiremos después de implementar el pago real (Stripe).
 ]
